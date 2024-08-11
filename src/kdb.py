4e6fc6379cd0268617d389cd8831391aa629e786
antma <antma@users.noreply.github.com>
2024-08-11 16:28:42 +0300

    add md5 field to kifus table for avoiding duplicates

--- src/kdb.py
+++ src/kdb.py
@@ -1,5 +1,7 @@
 # -*- coding: UTF8 -*-
 
+import hashlib
+import logging
 import lzma
 import sqlite3
 
@@ -7,6 +9,11 @@ import shogi
 
 def _insert(table, a):
   return 'INSERT INTO ' + table + '(' + ', '.join(a) + ') VALUES (' +  ', '.join('?' * len(a)) + ')'
+
+def _md5(data):
+  m = hashlib.md5()
+  m.update(data)
+  return m.digest()
 
 class KifuDB:
   def __init__(self, database = ':memory:'):
@@ -29,18 +36,40 @@ class KifuDB:
   time_control text,
   moves integer,
   result integer,
+  md5 blob,
   data blob)''')
     c.close()
+  def find_game_by_kifu_md5(self, kifu_md5):
+    c = self._connection.cursor()
+    res = c.execute('SELECT rowid FROM kifus WHERE md5 == ?', (kifu_md5, ))
+    r = res.fetchone()
+    c.close()
+    if r is None:
+      return r
+    return r[0]
   def insert(self, data: str) -> bool:
+    b = bytes(data, 'UTF8')
+    kifu_md5 = _md5(b)
+    rowid = self.find_game_by_kifu_md5(kifu_md5)
+    if not rowid is None:
+      logging.debug('KIFU file has been already inserted in DB (rowid = %d).', rowid)
+      return False
     g = shogi.kifu.Game.parse(data)
     if g is None:
       return False
-    b = bytes(data, 'UTF8')
     data = lzma.compress(b)
-    fields = ['sente', 'gote', 'start_date', 'sente_rating', 'gote_rating', 'time_control', 'moves', 'result', 'data']
-    q = _insert('kifus', fields)
-    v = g.get_row_values(data, fields)
+    fields = ['sente', 'gote', 'start_date', 'sente_rating', 'gote_rating', 'time_control']
+    v = g.get_row_values_from_headers(fields)
+    fields.append('moves')
+    v.append(len(g.moves))
+    fields.append('result')
+    v.append(g.sente_points())
+    fields.append('md5')
+    v.append(kifu_md5)
+    fields.append('data')
+    v.append(data)
     c = self._connection.cursor()
+    q = _insert('kifus', fields)
     c.execute(q, v)
     self._connection.commit()
     c.close()
