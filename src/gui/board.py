548cbdbee630f38e98fc0a08e81e7689b4b99f1f
antma <antma@users.noreply.github.com>
2024-08-13 19:55:32 +0300

    CanvasBoard

--- src/gui/board.py
+++ src/gui/board.py
@@ -32,3 +32,31 @@ class ShogiBoard:
       self._labels[i]['image'] = p
       self._board[i] = new
 
+class CanvasBoard:
+  def _cell_coords(self, i):
+    row, col = divmod(i, 9)
+    col = 8 - col
+    return (col * (self._cell_width + 1), row * (self._cell_width + 1))
+  def __init__(self, parent_window, images: ShogiPiecesImages, cell_width: int):
+    self._parent = parent_window
+    self._images = images
+    self._cell_width = cell_width
+    self._board = [piece.FREE] * 81
+    s = (cell_width + 1) * 9
+    self._canvas = tk.Canvas(parent_window, height = s, width = s, bg = '#fff')
+    for i in range(81):
+      x1, y1 = self._cell_coords(i)
+      self._canvas.create_rectangle(x1, y1, x1 + (cell_width + 1), y1 + (cell_width + 1))
+    self.draw_position(Position())
+    self._canvas.pack()
+  def draw_position(self, pos: Position):
+    for i, (old, new) in enumerate(zip(self._board, pos.board)):
+      if old == new:
+        continue
+      if old != piece.FREE:
+        self._canvas.delete(f'cell{i}')
+      if new != piece.FREE:
+        p = self._images.get(new)
+        x1, y1 = self._cell_coords(i)
+        self._canvas.create_image(x1 + 1, y1 + 1, anchor = tk.N + tk.W, image = p, tag = f'cell{i}')
+      self._board[i] = new
