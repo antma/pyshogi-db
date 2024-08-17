caaac439b3b73a5ba6a27b2a28ff3fa1eaf22487
antma <antma@users.noreply.github.com>
2024-08-18 02:25:12 +0300

    different images for sente and gote king

--- src/gui/pieces.py
+++ src/gui/pieces.py
@@ -7,18 +7,20 @@ import shogi
 def _load_images(filename):
   a = {}
   d = dict(map(lambda t: (t[1], t[0]), filter(lambda t: not t[1] is None, enumerate(shogi.piece.ASCII_LONG_NAMES))))
+  d['king.sente'] = shogi.piece.KING
+  d['king.gote'] = -shogi.piece.KING
   with tarfile.open(filename, 'r|xz') as tar_png:
     for m in tar_png:
       if not m.isreg():
         continue
-      i = d.get(m.name.removesuffix('.png'), -1)
-      if i < 0:
+      i = d.get(m.name.removesuffix('.png'))
+      if i is None:
         continue
       with tar_png.extractfile(m) as g:
         image = Image.open(g)
         a[i] = ImageTk.PhotoImage(image)
-        #a[-i] = ImageTk.PhotoImage(image.rotate(180))
-        a[-i] = ImageTk.PhotoImage(image.transpose(Image.Transpose.ROTATE_180))
+        if abs(i) != shogi.piece.KING:
+          a[-i] = ImageTk.PhotoImage(image.transpose(Image.Transpose.ROTATE_180))
   return a
 
 class ShogiPiecesImages:
