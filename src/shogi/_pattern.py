b02da4c58518f04e1ab6da6f92d8892874767208
antma <antma@users.noreply.github.com>
2025-03-31 12:46:56 +0300

    empty cells in castle patterns

--- src/shogi/_pattern.py
+++ src/shogi/_pattern.py
@@ -8,8 +8,12 @@ from .position import Position
 
 class _PiecePattern:
   def __init__(self, piece_latin_letter: str, cell_pattern: str):
-    self._piece = piece.from_upper(piece_latin_letter)
-    assert self._piece > 0
+    if piece_latin_letter == ' ':
+      #fix for half mino castle
+      self._piece = piece.FREE
+    else:
+      self._piece = piece.from_upper(piece_latin_letter)
+      assert self._piece > 0
     self._list = list(map(cell.digital_parse, cell_pattern.split(',')))
   def match(self, pos: Position, side: int) -> bool:
     return any(pos.board[c if side > 0 else cell.swap_side(c)] == side * self._piece for c in self._list)
